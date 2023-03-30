bee952af8822a4ee1a3d7aae206b873eea296a4d
LiliKaori <lilikaori10@gmail.com>
2023-03-30 14:49:36 -0300

    Correções ESlint+migration

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,11 @@
+// store => cadastrar / adicionar
+// index => listar vários
+// show => listar apenas um
+// update => atualizar
+// delete => deletar
+
+// class UserController(
+//     store(request, response){
+
+//     }
+// )
--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,19 @@
 import { Router } from 'express'
-// import { v4 } from 'uuid'
+import { v4 } from 'uuid'
 
-// import User from './app/models/User.js'
+import User from './app/models/User.js'
 
 const routes = new Router()
 
 routes.get('/', async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Rodolfão',
+    email: 'rodolfo3@gmail.com',
+    password_hash: '123456789',
+  })
 
-  // const user = await User.create({
-  //   id: v4(),
-  //   name: 'Rodolfo',
-  //   email: 'rodolfo@gmail.com',
-  //   password_hash: '123456789',
-  // })
-
-  return response.json('Hello world')
+  return response.json(user)
 })
 
 export default routes
