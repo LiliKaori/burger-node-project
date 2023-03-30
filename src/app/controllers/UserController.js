8cfcecbf92001db1a38d598c79426280b0967c0d
LiliKaori <lilikaori10@gmail.com>
2023-03-30 16:14:12 -0300

    Cofigurar cadastro de usuário e instalar Yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,9 +3,43 @@
 // show => listar apenas um
 // update => atualizar
 // delete => deletar
+import { v4 } from 'uuid'
+import * as Yup from 'yup'
+import User from '../models/User.js'
 
-// class UserController(
-//     store(request, response){
+class UserController {
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password_hash: Yup.string().required().min(6),
+      admin: Yup.boolean(),
+    })
 
-//     }
-// )
+    // if (!(await schema.isValid(request.body))) {
+    //   return response
+    //     .status(400)
+    //     .json({ error: 'Make sure your data is correct' })
+    // }
+
+    try {
+      await schema.validateSync(request.body, { abortEarly: false })
+    } catch (err) {
+      return response.status(400).json({ error: err.errors })
+    }
+
+    const { name, email, password_hash, admin } = request.body
+
+    const user = await User.create({
+      id: v4(),
+      name,
+      email,
+      password_hash,
+      admin,
+    })
+
+    return response.status(201).json({ id: user.id, name, email, admin })
+  }
+}
+
+export default new UserController()
--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,8 @@
 import { Router } from 'express'
-import { v4 } from 'uuid'
-
-import User from './app/models/User.js'
+import UserController from './app/controllers/UserController.js'
 
 const routes = new Router()
 
-routes.get('/', async (request, response) => {
-  const user = await User.create({
-    id: v4(),
-    name: 'Rodolfão',
-    email: 'rodolfo3@gmail.com',
-    password_hash: '123456789',
-  })
-
-  return response.json(user)
-})
+routes.post('/users', UserController.store)
 
 export default routes
